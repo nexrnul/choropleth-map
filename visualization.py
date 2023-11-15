46a5507f1d0cd59927d959d9d3a496724b8a4341
nexer nul <redpenguin4luca26@gmail.com>
2023-11-15 21:19:29 +0000

    map with classes data

---
+++ visualization.py
@@ -0,0 +1,21 @@
+import plotly.graph_objects as go
+
+import pandas as pd
+
+df = pd.read_csv('https://github.com/marestaing/hosting/blob/main/visited_states.csv?raw=true') 
+#importing google form (csv utilzed because of ability to store numbers/text data entires with ease) into plotly 
+
+fig = go.Figure(data=go.Choropleth(
+    locations=df['code'], # Spatial coordinates
+    z = df['number students'].astype(float), # Data to be color-coded
+    locationmode = 'USA-states', # set of locations match entries in `locations`
+    colorscale = 'PuBu', #sequential color scale due to graph format
+    colorbar_title = "number of students who visited specific state:", #key for color scaling
+))
+
+fig.update_layout(
+    title_text = 'APCSP America Choropleth Map', #general title
+    geo_scope='usa', # limits map scope to USA
+)
+
+fig.show() 
